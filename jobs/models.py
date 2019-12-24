dd00d9ee02867cbe2dae808dfb8ac4c4550b9bba
milemik <milemik68@gmail.com>
2019-12-24 23:18:28 +0100

    Second commit

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+# DATABASE!!!!!
+# U dokumentaciji django model field refere
+
+class Job(models.Model):
+	# Dodavalje slike u bazu podataka
+	image = models.ImageField(upload_to='images/')
+	# opis posla
+	description = models.CharField(max_length=200)
