b0ce000407b5763b0505c0d787a51f0b888b9ab8
milemik <milemik68@gmail.com>
2019-12-25 17:33:01 +0100

    Blog is ON

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+	# Dodavalje slike u bazu podataka
+	blog_name = models.CharField(max_length=20)
+	blog_date = models.DateTimeField()
+	blog_image = models.ImageField(upload_to='images/')
+	# opis posla
+	blog_description = models.TextField()
