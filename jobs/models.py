1aa7d2db86f0840c3d50bef6c288ba3f99aec2a3
milemik <milemik68@gmail.com>
2019-12-26 23:46:53 +0100

    We are getting closer

--- jobs/models.py
+++ jobs/models.py
@@ -5,7 +5,9 @@ from django.db import models
 # U dokumentaciji django model field refere
 
 class Job(models.Model):
+	# Naziv
+	jtitle = models.CharField(max_length=100)
 	# Dodavalje slike u bazu podataka
 	image = models.ImageField(upload_to='images/')
 	# opis posla
-	description = models.CharField(max_length=200)
+	description = models.TextField()
