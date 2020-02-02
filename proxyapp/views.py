7c5677ea2b7fc668c4731eba75e3876f8b654909
milemik <milemik68@gmail.com>
2020-02-02 09:50:16 +0100

    Going for it

--- proxyapp/views.py
+++ proxyapp/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from .models import Prox
 
 def prox(request):
-	proxies = Prox.objects
-	return render(request, 'proxyapp/proxy.html', {'proxies': proxies})
+	proxies = Prox.objects.all()
+	if len(proxies) == 0:
+		error = 'UPS! SORRY FOR THIS, THIS PAGE IS IN WORKING PROCESS'
+	return render(request, 'proxyapp/proxy.html', {'proxies': proxies, 'error': error})
