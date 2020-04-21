4dac0bad2dadd34251cfd1bb510e3bb0c60fb951
ivan_miletic <ivan@qpreems.com>
2020-04-21 13:43:50 +0200

    added Forms.py

---
+++ nalozi/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ('email',)
+
+    def save(self, commit=True):
+        user = super().save(False)
+        user.email = user.username
+        user = super().save()
+
+        return user
