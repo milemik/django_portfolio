35c0077a0b7aea2b132163dcdc1da8a1add6db2c
Ivan Miletic <ivanm@ernieapp.com>
2022-04-25 13:40:35 +0200

    Added test for invalid email domain

--- tests/test_nalozi.py
+++ tests/test_nalozi.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from django.test import Client
 
 from tests.factories import ClientJobsFactory
 
@@ -26,7 +27,6 @@ def test_profile_with_job(auth_client):
     assert response.context["jobs"].count() == 1, "Expect jobs queryset shows 1 job"
 
 
-
 @pytest.mark.django_db
 def test_profile_admin(admin_client):
     job_num = 3
@@ -38,3 +38,12 @@ def test_profile_admin(admin_client):
     assert response.status_code == 200, "Expect OK response"
     assert response.context["jobs"].count() == job_num, "Expect jobs queryset show all jobs"
 
+
+@pytest.mark.django_db
+def test_singup_wrong_email_domain():
+    data = {"username": "testuser", "email": "test@test.com", "password1": "somepass123", "password2": "somepass123"}
+    client = Client()
+    url = reverse("singup")
+    response = client.post(url, data)
+    assert response.status_code == 200
+    assert response.context.get("message") == "Domain not valid"
