10901e28440417a5b6705af28b9bd8a0d2a50c87
mileta <milemik68@gmail.com>
2022-09-30 17:08:48 +0200

    test_blogs - test_blog_detail_view_blog_doesnt_exist

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -3,8 +3,16 @@ from django.urls import reverse
 
 
 @pytest.mark.django_db
-def test_blog_init(client):
+def test_all_blogs_view(client):
     url = reverse("allblogs")
     response = client.get(url)
 
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_blog_detail_view_blog_doesnt_exist(client):
+    url = reverse("blog_detail", args=(1,))
+    response = client.get(url)
+
+    assert response.status_code == 404
